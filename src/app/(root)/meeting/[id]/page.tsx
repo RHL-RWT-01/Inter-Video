99465bdd771f8d77d67dfc02f21f1e95f5b04cc4
Rahul Rawat <rahulrawat4060@gmail.com>
2025-05-07 16:18:41 +0530

    implemented meetings page

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+"use client"
+
+import Loader from '@/components/Loader';
+import MeetingRoom from '@/components/MeetingRoom';
+import MeetingSetup from '@/components/MeetingSetup';
+import useGetCallById from '@/hooks/useGetCallById';
+import { useUser } from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import { useParams } from 'next/navigation'
+import React, { useState } from 'react'
 
 function MeetingPage() {
+  const { id } = useParams();
+  const { isLoaded } = useUser();
+  const [isSetUpCompleted, setIsSetUpCompleted] = useState<boolean>(false);
+  const { call, isCallLoading } = useGetCallById(id);
+  if (!isLoaded || isCallLoading) return <Loader />
+  if(!call){
+    return(
+      <div className='h-screen flex items-center justify-center'>
+        <p className='text-2xl font-semibold'>Meeting not found</p>
+      </div>
+    );
+  }
   return (
-    <div>meetingPage</div>
+    <StreamCall call={call}>
+      <StreamTheme>
+        (!isSetUpCompleted ?(
+        <MeetingSetup onSetupComplete={() => setIsSetUpCompleted(true)} />
+        ):(<MeetingRoom />));
+      </StreamTheme>
+    </StreamCall>
   )
 }
 
